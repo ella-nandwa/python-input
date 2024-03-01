4a8b5ae29d1a169bcd03415cfd45ccead6951127
ella-nandwa <ellanandwa254@gmail.com>
2024-03-02 01:11:09 +0300

    user_input.py

---
+++ user_input.py
@@ -0,0 +1,12 @@
+# Ask for user's name
+name = input("What is your name? ")
+
+# Ask for user's age
+age = input("How old are you? ")
+
+# Ask for user's location
+location = input("Where do you live? ")
+
+# Print personalized message
+print("Hello Ella, you are 22 years old and live in Nairobi.")
+
